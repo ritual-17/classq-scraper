b3f74c9363da2a1acc02130b82c2b30541cfff00
ethan <pattee14@mcmaster.ca>
2023-07-25 00:45:47 -0400

    separated each scraper into its own func. updated Section struct. Fixed error where <a> tag on sections with 'Section Comments' would have their href used instead of the desired 'Section' href. Scraping 'Seats Remaining' info works majority of the time but a few sections will randomly not have it and the sections that dont have it change everytime its run.

--- scraper.go
+++ scraper.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
 
@@ -11,16 +13,25 @@ import (
 )
 
 type Section struct {
-	status   string
-	section  string
-	activity string
-	term     string
-	days     string
-	start    string
-	end      string
+	subject        string
+	course         string
+	status         string
+	section        string
+	activity       string
+	term           string
+	days           string
+	start          string
+	end            string
+	seatsURL       string
+	seatsRemaining string
 }
 
+var sections []Section = []Section{}
+
 func main() {
+
+	start := time.Now()
+
 	f, err := os.Create("file.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -28,9 +39,114 @@ func main() {
 
 	defer f.Close()
 
+	StartScraping()
+
+	for _, section := range sections {
+		f.WriteString("Subject: " + section.subject + "\n")
+		f.WriteString("Course: " + section.course + "\n")
+		f.WriteString("Status: " + section.status + "\n")
+		f.WriteString("Section: " + section.section + "\n")
+		f.WriteString("Type: " + section.activity + "\n")
+		f.WriteString("Term: " + section.term + "\n")
+		f.WriteString("Days: " + section.days + "\n")
+		f.WriteString("Start Time: " + section.start + "\n")
+		f.WriteString("End Time: " + section.end + "\n")
+		f.WriteString("URL: " + section.seatsURL + "\n")
+		f.WriteString("Seats Remaining: " + section.seatsRemaining + "\n")
+		f.WriteString("-----------------------------------\n")
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Time elapsed: %s", elapsed)
+
+}
+
+func StartScraping() {
 	subjects := colly.NewCollector(colly.AllowedDomains("courses.students.ubc.ca"))
+	count := 0
+
+	//main page scraper that finds all subjects
+	subjects.OnHTML("tr[class=section1], tr[class=section2]", func(h *colly.HTMLElement) {
+		var section Section
+		count++
+		//for testing, only looking at Anthropology
+		if count != 8 {
+			return
+		}
+
+		section.subject = h.ChildText("tr td:nth-of-type(1)")
+
+		var coursePage string
+		h.ForEach("a", func(_ int, e *colly.HTMLElement) {
+			coursePage = e.Attr("href")
+		})
+
+		if coursePage == "" {
+			return
+		}
+
+		ScrapeCoursePage(h.Request.AbsoluteURL(coursePage), section)
+
+	})
+	subjects.Visit("https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-all-departments")
+
+}
+
+func ScrapeCoursePage(url string, section Section) {
 	courses := colly.NewCollector(colly.AllowedDomains("courses.students.ubc.ca"))
+
+	courses.OnHTML("tr[class=section1], tr[class=section2]", func(h *colly.HTMLElement) {
+		var sectionsPage string
+		h.ForEach("a", func(_ int, e *colly.HTMLElement) {
+			section.course = e.Text
+			sectionsPage = e.Attr("href")
+		})
+
+		if sectionsPage == "" {
+			return
+		}
+
+		ScrapeSectionPage(h.Request.AbsoluteURL(sectionsPage), section)
+
+	})
+
+	courses.Visit(url)
+
+}
+
+func ScrapeSectionPage(url string, section Section) {
 	sections := colly.NewCollector(colly.AllowedDomains("courses.students.ubc.ca"))
+
+	sections.OnHTML("tr[class=section1], tr[class=section2]", func(h *colly.HTMLElement) {
+
+		section.status = h.ChildText("tr td:nth-of-type(1)")
+		section.section = h.ChildText("tr td:nth-of-type(2)")
+		section.activity = h.ChildText("tr td:nth-of-type(3)")
+		section.term = h.ChildText("tr td:nth-of-type(4)")
+		section.days = h.ChildText("tr td:nth-of-type(7)")
+		section.start = h.ChildText("tr td:nth-of-type(8)")
+		section.end = h.ChildText("tr td:nth-of-type(9)")
+
+		if section.status == "" {
+			section.status = "Open"
+		}
+
+		seatsPage := h.ChildAttr("tr td:nth-of-type(2) a", "href")
+
+		if seatsPage == "" {
+			return
+		}
+
+		section.seatsURL = h.Request.AbsoluteURL(seatsPage)
+		ScrapeSeatsPage(section.seatsURL, section)
+
+	})
+
+	sections.Visit(url)
+
+}
+
+func ScrapeSeatsPage(url string, section Section) {
 	seats := colly.NewCollector(colly.AllowedDomains("courses.students.ubc.ca"))
 
 	seats.OnHTML("tbody", func(h *colly.HTMLElement) {
@@ -41,96 +157,11 @@ func main() {
 		}
 
 		seatsRemaining := h.DOM.Children().Eq(0).Text()
-		f.WriteString(seatsRemaining + "\n")
+		section.seatsRemaining = seatsRemaining
 
 	})
 
-	sections.OnHTML("tr[class=section1], tr[class=section2]", func(h *colly.HTMLElement) {
-		section := Section{
-			status:   h.ChildText("tr td:nth-of-type(1)"),
-			section:  h.ChildText("tr td:nth-of-type(2)"),
-			activity: h.ChildText("tr td:nth-of-type(3)"),
-			term:     h.ChildText("tr td:nth-of-type(4)"),
-			days:     h.ChildText("tr td:nth-of-type(7)"),
-			start:    h.ChildText("tr td:nth-of-type(8)"),
-			end:      h.ChildText("tr td:nth-of-type(9)"),
-		}
+	seats.Visit(url)
 
-		if section.status == "" {
-			section.status = "Open"
-		}
-		f.WriteString("Section:\n")
-		f.WriteString(section.status + "\n")
-		f.WriteString(section.section + "\n")
-		f.WriteString(section.activity + "\n")
-		f.WriteString(section.term + "\n")
-		f.WriteString(section.days + "\n")
-		f.WriteString(section.start + "\n")
-		f.WriteString(section.end + "\n")
-
-		var seatsPage string
-		h.ForEach("a", func(_ int, e *colly.HTMLElement) {
-			seatsPage = e.Attr("href")
-		})
-
-		if seatsPage == "" {
-			return
-		}
-
-		seats.Visit(h.Request.AbsoluteURL(seatsPage))
-
-	})
-
-	//course page scraper that finds all courses
-	courses.OnHTML("tr[class=section1], tr[class=section2]", func(h *colly.HTMLElement) {
-		var sectionsPage string
-		h.ForEach("a", func(_ int, e *colly.HTMLElement) {
-			_, err := f.WriteString(e.Text + "\n")
-			if err != nil {
-				log.Fatal(err)
-			}
-			sectionsPage = e.Attr("href")
-		})
-
-		if sectionsPage == "" {
-			return
-		}
-
-		sections.Visit(h.Request.AbsoluteURL(sectionsPage))
-
-	})
-
-	count := 0
-
-	//main page scraper that finds all subjects
-	subjects.OnHTML("tr[class=section1], tr[class=section2]", func(h *colly.HTMLElement) {
-		count++
-		//for testing, only looking at Anthropology
-		if count != 8 {
-			return
-		}
-
-		subject := h.ChildText("tr td:nth-of-type(1)")
-		_, err := f.WriteString(subject + "\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-		var coursePage string
-		h.ForEach("a", func(_ int, e *colly.HTMLElement) {
-			coursePage = e.Attr("href")
-		})
-
-		if coursePage == "" {
-			return
-		}
-		_, err = f.WriteString("COURSES:\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		courses.Visit(h.Request.AbsoluteURL(coursePage))
-
-	})
-	subjects.Visit("https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-all-departments")
-
+	sections = append(sections, section)
 }
